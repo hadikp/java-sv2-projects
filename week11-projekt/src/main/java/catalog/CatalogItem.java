ed4fcb2330a4ae0ad17e2923285daa82121b721d
hadikp <hp20001025@gmail.com>
2022-01-06 12:16:30 +0100

    CatalogItem ready

--- week11-projekt/src/main/java/catalog/CatalogItem.java
+++ week11-projekt/src/main/java/catalog/CatalogItem.java
@@ -16,11 +16,32 @@ public class CatalogItem {
     }
 
     public int fullLengthAtOneItem() {
-        return 11;
+        int lengthSum = 0;
+        for (Feature f: features) {
+            if (f instanceof AudioFeatures) {
+                lengthSum += ((AudioFeatures) f).getLength();
+            }
+        }
+        return lengthSum;
     }
 
     public List<String> getContributors() {
-        return null;
+        List<String> allContributors = new ArrayList<>();
+        for (Feature f: features) {
+            listAudioContributors(allContributors, f);
+            if (f instanceof PrintedFeatures) {
+                List<String> printedContributors = ((PrintedFeatures) f).getContributors();
+                allContributors.addAll(printedContributors);
+            }
+        }
+        return allContributors;
+    }
+
+    private void listAudioContributors(List<String> allContributors, Feature f) {
+        if (f instanceof AudioFeatures) {
+            List<String> audioContributors = ((AudioFeatures) f).getContributors();
+            allContributors.addAll(audioContributors);
+        }
     }
 
     public List<String> getTitles() {
@@ -28,15 +49,31 @@ public class CatalogItem {
     }
 
     public boolean hasAudioFeature() {
-        return true;
+        for (Feature f: features) {
+            if (f instanceof AudioFeatures) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean hasPrintedFeature() {
-        return true;
+        for (Feature f: features) {
+            if (f instanceof PrintedFeatures) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int numberOfPagesAtOneItem() {
-        return 11;
+        int pages = 0;
+        for (Feature f: features) {
+            if (f instanceof PrintedFeatures) {
+                 pages = ((PrintedFeatures) f).getNumberOfPages();
+            }
+        }
+       return pages;
     }
 
     public List<Feature> getFeatures() {
